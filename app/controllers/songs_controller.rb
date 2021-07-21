6cc5f7598203c002f942c2b6564a22a92a79ab64
VandelayUtd <drewhadley.guitar@gmail.com>
2021-07-21 16:29:47 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,24 @@
 class SongsController < ApplicationController
     get '/songs' do
+        @songs = Song.all 
         erb :"/songs/index"
     end
+
+    get '/songs/new' do
+        
+        erb :"/songs/new"
+    end
+
+    get '/songs/:slug' do
+        @slug = Song.find_by_slug(params[:slug])
+        erb :"/songs/show"
+    end
+
+    post '/songs' do 
+    
+        redirect to "/songs"
+    end
+
 
   
 end
